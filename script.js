aaee8cf1653fe43571e495acee0a9ad741edaf50
KT Duffy <ktduffyinc@gmail.com>
2023-11-25 17:08:08 -0600

    basic page setup

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+$(".x").hide();
+
+$(".info").on("click", function(){
+    $("iframe").hide("slow");
+    $("#infoPage").show();
+    $(".x").fadeIn();
+    console.log("works");
+});
+
+$(".x").on("click", function(){
+    $("iframe").fadeIn("slow");
+});
+
+});
